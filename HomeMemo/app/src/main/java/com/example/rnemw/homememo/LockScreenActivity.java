2b0b6cff21613e5b7a463c03206f3faed5787bd3
DESKTOP-LFU7QJK\rnemw <rnemwkqm9@gmail.com>
2016-12-20 14:33:07 +0900

    잠금화면 구현 완료

--- HomeMemo/app/src/main/java/com/example/rnemw/homememo/LockScreenActivity.java
+++ HomeMemo/app/src/main/java/com/example/rnemw/homememo/LockScreenActivity.java
@@ -2,17 +2,30 @@ package com.example.rnemw.homememo;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
 
 /**
  * Created by rnemw on 2016-12-19.
  */
 
 public class LockScreenActivity extends AppCompatActivity {
+    private Button releaseBtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.lock_screen);
+
+        releaseBtn = (Button)findViewById(R.id.lockReleaseBtn);
+        releaseBtn.setOnLongClickListener(new Button.OnLongClickListener(){
+            public boolean onLongClick(View v){
+                finish();
+                return true;
+            }
+        });
+
 
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED
                 | WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD);
--- HomeMemo/app/src/main/java/com/example/rnemw/homememo/MainActivity.java
+++ HomeMemo/app/src/main/java/com/example/rnemw/homememo/MainActivity.java
@@ -1,13 +1,42 @@
 package com.example.rnemw.homememo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button onBtn, offBtn;
+    Intent onIntent, offIntent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        onBtn= (Button)findViewById(R.id.OnBtn);
+        offBtn= (Button)findViewById(R.id.OffBtn);
+
+        BtnOnClickListener onClickListener = new BtnOnClickListener();
+        onBtn.setOnClickListener(onClickListener);
+        offBtn.setOnClickListener(onClickListener);
+
+    }
+
+    class BtnOnClickListener implements Button.OnClickListener {
+        @Override
+        public void onClick(View view) {
+            switch (view.getId()) {
+                case R.id.OnBtn:
+                    onIntent = new Intent(MainActivity.this, ScreenService.class);
+                    startService(onIntent);
+                    break;
+                case R.id.OffBtn:
+                    offIntent = new Intent(MainActivity.this, ScreenService.class);
+                    stopService(offIntent);
+                    break;
+            }
+        }
     }
 }
